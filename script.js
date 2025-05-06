00f15006f569619040af3ad45062a1bbb02e61ed
Igor <68581590+igorcalabraro@users.noreply.github.com>
2025-05-06 16:28:21 -0300

    feat: theme selector identified by class

--- script.js
+++ script.js
@@ -2,4 +2,12 @@ $(document).ready(function() {
   $('.aside-menu').on('click', function() {
     $('#sidebar').toggleClass('collapsed');
   });
+
+  $('#light-mode').on('click', function() {
+    $('html').removeClass('dark')
+  })
+
+  $('#dark-mode').on('click', function() {
+    $('html').addClass('dark')
+  })
 })
